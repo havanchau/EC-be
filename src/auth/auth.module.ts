3756a7597c51be075d1241ff4b5a5b279c6c0b66
havanchau <havanchau@hanbiro.vn>
2024-10-30 08:25:02 +0700

    initial commit

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { Auth, AuthSchema } from './auth.schema';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Auth.name, schema: AuthSchema }]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secretKey',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
