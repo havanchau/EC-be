00ded2574ef85303bf932cb167d6b8b92f568b9a
havanchau <havanchau@hanbiro.vn>
2024-10-31 15:37:45 +0700

    [chore] fix deployment error

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { ImageModule } from './cloudinary/image.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { ProductModule } from './product/product.module';
     AuthModule,
     UserModule,
     ProductModule,
+    ImageModule,
   ],
   controllers: [AppController],
   providers: [AppService],
