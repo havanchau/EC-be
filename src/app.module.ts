edef372da7fd369f9e0273420567e3012618b744
havanchau <havanchau@hanbiro.vn>
2024-11-01 14:45:53 +0700

    [chore] add key for gen JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,16 @@ import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
 import { ImageModule } from './cloudinary/image.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'supersecretkey1234567890',
+      signOptions: { expiresIn: '30d' },
     }),
     MongooseModule.forRoot(process.env.DB_CONNECTION_STRING),
     AuthModule,
