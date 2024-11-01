914476c485c019c36e5a2e81ab675de3097385df
havanchau <havanchau@hanbiro.vn>
2024-11-01 13:49:05 +0700

    [chore] add CloudinaryModule to product module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -4,10 +4,12 @@ import { Product, ProductSchema } from './product.schema';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { ImageService } from 'src/cloudinary/image.service';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+    CloudinaryModule,
   ],
   controllers: [ProductController],
   providers: [ProductService, ImageService],
