ec61215ac9f911654e7b7c02b31e00aab29ba0fa
havanchau <chauhvc2.dy@gmail.com>
2024-11-01 19:05:34 +0700

    [chore] fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+  
   app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
