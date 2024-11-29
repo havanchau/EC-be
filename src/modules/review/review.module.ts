e0027d77b504028bc307b01c8ccfe6b77d4fdc5b
havanchau <chauhvc2.dy@gmail.com>
2024-11-29 09:48:50 +0700

    [fix] fix eror deploy

--- src/modules/review/review.module.ts
+++ src/modules/review/review.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Review, ReviewSchema } from './review.schema';
 import { ReviewService } from './review.service';
@@ -8,7 +8,7 @@ import { ProductModule } from '../product/product.module';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Review.name, schema: ReviewSchema }]),
-    ProductModule,
+    forwardRef(() => ReviewModule)
   ],
   controllers: [ReviewController],
   providers: [ReviewService],
