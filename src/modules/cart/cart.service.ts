4d3e3090d03a295139689241376d28fe47a38572
havanchau <chauhvc2.dy@gmail.com>
2024-12-05 05:56:27 +0700

    [fix] error cart api

--- src/modules/cart/cart.service.ts
+++ src/modules/cart/cart.service.ts
@@ -21,13 +21,15 @@ export class CartService {
             .findOne({ userId })
             .populate('userId')
             .exec();
+
+        let cart = null;
     
         if (oldCart) {
             Object.assign(oldCart, cartData);
     
             const updatedCart = await oldCart.save();
     
-            return { cart: updatedCart };
+            cart = updatedCart
         } else {
             const newCart = new this.cartModel({
                 ...cartData,
@@ -35,11 +37,32 @@ export class CartService {
             });
     
             const result = await newCart.save();
-            return { cart: result };
+            cart = result
         }
+
+
+        const productIds = cart?.items.map((item: any) => (item.productId as string));
+        const products = await this.productService.findAll({ productIds: productIds });
+
+        const productMap = cart?.items.map((item: any) => ({
+            productId: item.productId._id.toString(),
+            quantity: item.quantity,
+        }))
+
+        const productsInfo = products.map((product: any) => {
+            const orderDetail = productMap.find((item: any) => item.productId === product._id.toString());
+            if (orderDetail) {
+                return {
+                    ...product,
+                    quantity: orderDetail.quantity,
+                    totalPrice: orderDetail.quantity * product.price,
+                };
+            }
+            return product;
+        });
+
+        return {cart, productsInfo};
     }
-    
-    
 
     async getCart(userId: string): Promise<any> {
         const cart = await this.cartModel
@@ -70,29 +93,4 @@ export class CartService {
         return {cart, productsInfo};
     }
 
-    async getCartById(id: string): Promise<any> {
-        const cart = await this.cartModel
-            .findById(id)
-            .populate('userId')
-            .exec();
-        if (!cart) {
-            throw new NotFoundException(`Cart with ID ${id} not found`);
-        }
-
-
-
-        return cart;
-    }
-
-    async updateCart(id: string, cartData: Partial<CreateCartDto>): Promise<Cart> {
-        const cart = await this.cartModel
-            .findByIdAndUpdate(id, cartData)
-            .exec();
-
-        if (!cart) {
-            throw new NotFoundException(`Cart with ID ${id} not found`);
-        }
-
-        return cart;
-    }
 }
